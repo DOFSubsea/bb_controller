14f43a0363220b42997ebdbcf00f87f5f8410aaf
Ryan Ledford <ryan.ledford@dofsubsea.com>
2017-03-07 12:53:15 +0000

    updated start script

--- start_bb_controller.sh
+++ start_bb_controller.sh
@@ -2,11 +2,9 @@
 #service should have its working directory set to /home/debian/bb_controller
 cd /home/debian/bb_controller
 echo "*** Begin Startup $(date) ***" > startup.log
-echo "Waiting to ensure all resources are available..." >> startup.log
-sleep 15
 /usr/bin/tmux new-session -d -s server "/usr/local/bin/nodemon server"
 echo "Started nodemon: $(date)" >> startup.log
-sleep 15
+sleep 10
 /usr/bin/tmux new-session -d -s update "/usr/bin/watch -n1800 git pull origin master"
 echo "Started watch for GitHub updates: $(date)" >> startup.log
 echo "*** End Startup: $(date) *** " >> startup.log
